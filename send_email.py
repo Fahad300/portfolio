749ee2b1374145c713290faf185d0c7cd2a031e5
Fahad.Mushtaq <fahad.mushtaq300@gmail.com>
2023-12-12 20:13:41 +0500

    completed porfolio website

--- send_email.py
+++ send_email.py
@@ -4,7 +4,7 @@ def send_email(message):
     host = "smtp.gmail.com"
     port = 465
     usr = "faadeemian512@gmail.com"
-    pwd = "oyol hpwt rymx wjew"
+    pwd = "oyolhpwtrymxwjew"
     context = ssl.create_default_context()
     receiver = "faadeemian512@gmail.com"
     with smtplib.SMTP_SSL(host, port, context=context) as server:
